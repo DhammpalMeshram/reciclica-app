1b2cb1a541798593ae158f672672cd957d20e8b2
Dhammpal Meshram <dhammpal123321@gmail.com>
2024-11-03 16:07:40 +0530

    navigation added to all screens

--- src/app/pages/pickup-call/pickup-call.page.ts
+++ src/app/pages/pickup-call/pickup-call.page.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-pickup-call',
   templateUrl: './pickup-call.page.html',
@@ -7,9 +7,12 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PickupCallPage implements OnInit {
 
-  constructor() { }
+  constructor( private router: Router) { }
 
   ngOnInit() {
   }
 
+  newPickUpCall() {
+    this.router.navigate(['home']);
+  }
 }
